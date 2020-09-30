cb2d62d72ceabfeafdac7645b0c2ae98a2ce9cf9
Laura Cole <lauracole1900@comcast.net>
2020-09-30 17:53:10 -0600

    starter files and pseudocode

---
+++ script.js
@@ -0,0 +1,34 @@
+// provided in the css
+// body tag
+// textarea tag
+// jumbotron class
+// description class
+// time-block class
+// row class
+// hour class
+// past class
+// present class
+// future class
+// saveBtn class
+// saveBtn i:hover class -- I don't know how this works yet
+
+// display current date
+// display current time
+// create time-blocks for standard business hours
+// link time-blocks to current date & time
+// make time-blocks change color based on future, present and past time
+// user has ability to scroll through dates & times?
+// create user input form
+
+// user input needs:
+// date of appt
+// time of appt
+// name of appt
+// description of appt
+// save button that saves to local storage
+
+// user input form activated when user clicks on that time-block: event listener
+
+// user input once saved must then be placed in the calendar at the set time
+
+// saved events persist when page is refreshed
